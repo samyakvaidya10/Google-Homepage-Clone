558ccf37f0dffd0a66727640d4eb040f3b54d128
samyakvaidya10 <samyakmcv@gmail.com>
2022-10-17 19:25:05 +0530

    update dropdown button

--- src/script.js
+++ src/script.js
@@ -1,4 +1,15 @@
+var tableOpen=false;
+
 function showApps(){
-    document.getElementById("app-list-id").classList.toggle("show");
-    console.log("work")
+    // document.getElementById("app-list-id").classList.toggle("show");
+    document.getElementById("app-list-id").classList.toggle("show");   
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('#app-btn')) {
+  var myDropdown = document.getElementById("app-list-id");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
 }
